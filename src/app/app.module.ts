dc3304517f2ce6532f5fa8eb5fdfe0519421279b
Tomasz Fraczek <tom.frontz@gmail.com>
2022-08-06 11:52:35 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent, PostCreateComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
