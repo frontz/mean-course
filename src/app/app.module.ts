957837247cef068dee4e4d3b54188cd60811945c
Tomasz Fraczek <tom.frontz@gmail.com>
2022-08-06 11:16:55 +0200

    Dummy of post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
